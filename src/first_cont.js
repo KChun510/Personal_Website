1420366db942adad20596778a66a38aa44eaf262
Kyle Chun <kylechun1@gmail.com>
2023-07-31 18:18:29 -0700

    updating first cont

--- src/first_cont.js
+++ src/first_cont.js
@@ -69,7 +69,7 @@ const FirstCont = () =>{
                         html_append += "<small class='text'>"+ 
                         "<svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill ='orange' class='bi bi-dot' viewBox='0 0 16 16'>"+
                         "<path d='M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z'/>"+ 
-                        "</svg>Pyhton"+
+                        "</svg>Python"+
                         "</small>";
                         
                         break
