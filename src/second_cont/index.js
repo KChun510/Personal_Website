ff756f15575541129ab992f7a848f178a8992460
WestCoastToast <westcoasttoast@westcoaoastsmbp.mynetworksettings.com>
2024-09-04 20:53:04 -0400

    Huge cleanup, and backend improvement

--- src/second_cont/index.js
+++ src/second_cont/index.js
@@ -169,7 +169,7 @@ const SecondCont = () => {
                     <br></br>
 
                     <div className="container w-100 h-25 text-center">
-                        <a href="https://github.com/KChun510/Resume/blob/71a1f54c883668f63e285a51f33334c48684a2bb/Profile%20(4).pdf"
+                        <a href="https://docs.google.com/document/d/1fzqxueGqjhMUBqfE1Q12z9S2X3bAPbeKIDESDi2UlE4/edit?usp=sharing"
                             target="_blank" rel="noopener noreferrer">
                             <button type="button" className="btn btn-outline-primary btn-sm">Veiw full resume </button>
                         </a>
