1ddb33b928b33849807bfa52d5e745b5095db3eb
Kchun510 <kylechun1@gmail.com>
2024-11-12 16:36:50 -0800

    Updating my Language Icons

--- src/first_cont/components/langIcons/index.js
+++ src/first_cont/components/langIcons/index.js
@@ -57,9 +57,20 @@ const langIcons = {
         </svg>Shell
     </small>
     ),
+    C: (<small className='text'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='gray' className='bi bi-dot' viewBox='0 0 16 16'>
+            <path d='M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z' />
+        </svg>C
+    </small>
+    ),
+    Assembly: (<small className='text'>
+        <svg xmlns='http://www.w3.org/2000/svg' width='30' height='30' fill='brown' className='bi bi-dot' viewBox='0 0 16 16'>
+            <path d='M8 9.5a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z' />
+        </svg>Assembly
+    </small>
+    ),
 
     null: null
-
 }
 
 function CreateIcon({ lang }) {
