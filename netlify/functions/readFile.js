10f72ff2e446bb032ecae6602edd6022ff687a07
Kchun510 <kylechun1@gmail.com>
2024-12-31 16:18:46 -0800

    New path solution

--- netlify/functions/readFile.js
+++ netlify/functions/readFile.js
@@ -7,7 +7,9 @@ exports.handler = async () => {
 	try {
 		// Resolve path to the JSON file (make sure to adjust the path to your actual file)
 		//		const filePath = path.join(process.cwd(), 'data', 'gitQueryLog.json');
-		const filePath = require.resolve('./gitQueryLog.json'); // Relative path to the JSON file within the function's folder
+		//		const filePath = require.resolve('./gitQueryLog.json'); // Relative path to the JSON file within the function's folder
+		const filePath = path.join(__dirname, 'gitQueryLog.json');
+
 
 		//	const filePath = path.resolve(__dirname, '..', 'data', 'gitQueryLog.json')
 		const fileContents = fs.readFileSync(filePath, 'utf8');
